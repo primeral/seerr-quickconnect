96a8fdadb61580a14b2e89638bba78d88077ef48
root <root@t5810.algran.de>
2026-06-11 16:29:02 -0500

    Add mounted path import for login redirects

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -2,6 +2,7 @@ import type { User } from '@app/hooks/useUser';
 import { useUser } from '@app/hooks/useUser';
 import { useRouter } from 'next/dist/client/router';
 import { useEffect, useRef } from 'react';
+import { withMountedPath } from '@app/utils/mountedPath';
 
 interface UserContextProps {
   initialUser: User;
